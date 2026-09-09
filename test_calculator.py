21130287402641070e54e5ceeaba9a4e08bc0139
Adhya Bhagat <adhyabhagat716@gmail.com>
2026-09-09 22:21:57 +0530

    Introduce intentional test failure

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ import pytest
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add(10,5) == 15
+    assert add(10,5) == 20
 
 def test_subtract():
     assert subtract(10,5) == 5  
